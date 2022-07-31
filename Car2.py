7cabcd3a7901c3e7feaac6ff26d5015ebf06053b
DiegoBran16 <31348574+DiegoBran16@users.noreply.github.com>
2022-07-31 10:57:15 -0600

    Update Car2.py

--- Car2.py
+++ Car2.py
@@ -74,40 +74,40 @@ class Tracker:
         #Función ciclica que se ejecuta por cada elemento de la lista de autos detectados por la red. 
         
         for i in info:
-            x1,y1,x2,y2 = i # 
-            centro_y = (y2 + y1)//2
-            centro_x = (x2 + x1)//2
+            x1,y1,x2,y2 = i # se recuperan las coordenadas del cuadro delimitador 
+            centro_y = (y2 + y1)//2 #Se obtiene la coordenada del centroide en y 
+            centro_x = (x2 + x1)//2 #Se obtiene la coordenada del centroide en x 
             
 
-            validador = False
+            validador = False #Variable para validar si el auto fue detectado ateriormente
           
             
-            for id, obj_carro in self.autos_detectados.items():
-                id_carro, info_carro = obj_carro.get_variables()
-                centro_x_auto, centro_y_auto, x1_auto, y1_auto, x2_auto, y2_auto = info_carro[len(info_carro)-1]
-                recorrido = np.sqrt((centro_x-centro_x_auto)**2+(centro_y-centro_y_auto)**2)
-                cornerDistance= int(np.sqrt((x2_auto-x1_auto)**2+(y2_auto-y1_auto)**2))
+            for id, obj_carro in self.autos_detectados.items(): #Se ejecuta el ciclo para cada elemento del diccionario 
+                id_carro, info_carro = obj_carro.get_variables() # se obtienen el ID y la lista que contiene los centroides y las coordenadas del cuadro delimitador. 
+                centro_x_auto, centro_y_auto, x1_auto, y1_auto, x2_auto, y2_auto = info_carro[len(info_carro)-1] #Se obteien los centroides y las coordenadas del cuadro delimitador del ultimo registro en la lista
+                recorrido = np.sqrt((centro_x-centro_x_auto)**2+(centro_y-centro_y_auto)**2) # Se obtiene la distancia euclidiana entre las coordenadas actuales de los centroides y las coordenadas del ultimo registro de la lista de centroides.
+                cornerDistance= int(np.sqrt((x2_auto-x1_auto)**2+(y2_auto-y1_auto)**2)) # Se obtiene la distancia entre las coordenadas del cuadro delimirador y se divide entre 2
                
-
-                if  (recorrido <=100) and abs(centro_y_auto - centro_y) < cornerDistance//2:
-                    info_aux = centro_x, centro_y,x1,y1,x2,y2 
-                    obj_carro.agregar_info(info_aux)
-                    self.centroides[id] = (centro_x, centro_y)
-                    obj_carro.set_centroides(centro_x,centro_y)
+                 #Se valida que la distancia euclidiana sea menor a 100 pixeles y a la mitad de la distancia entre las coordenadas 
+                if  (recorrido <=100) and abs(centro_y_auto - centro_y) < cornerDistance//2: 
+                    info_aux = centro_x, centro_y,x1,y1,x2,y2  # se agregan a la lista info_aux las coordenadas de los centroides y las coordenadas del cuadro delimitador
+                    obj_carro.agregar_info(info_aux) # se agregan las coordenadas los centroides y las coordenadas del cuadro delimitador al objeto carro
+                    self.centroides[id] = (centro_x, centro_y) # Se agregan los centroides al diccionario de ventroides según el ID 
+                    obj_carro.set_centroides(centro_x,centro_y) # Se agregan los centroides a las listas de centroides auxiliares  y se agrega el tiempo a la lista de tiempos 
                     validador = True
         
                     break
          
         
-            if validador is False and y2 <300:
-                carro = Auto() 
-                carro.asignar_id(self.car_id)
-                info_objeto = centro_x, centro_y,x1,y1,x2,y2
-                carro.agregar_info(info_objeto)
-                self.centroides[self.car_id] = (centro_x,centro_y)
-                self.autos_detectados[self.car_id] = (carro) 
-                self.car_id = self.car_id +1
+            if validador is False and y2 <300: # Si el validador es falso y la coordenada vertical de la esquina inferior derecha supera 300 pixeles se define como un auto nuevo en el diccionario 
+                carro = Auto() # se crea un objeto auto
+                carro.asignar_id(self.car_id) # Se asigna un ID numerico
+                info_objeto = centro_x, centro_y,x1,y1,x2,y2 # se agregan las coordenadas de los centroides y las coordenadas del cuadro delimitador a una variable 
+                carro.agregar_info(info_objeto)              # Las coordenadas en la variable se agregan a la lista del objeto 
+                self.centroides[self.car_id] = (centro_x,centro_y). # Se agregan las coordenadas de los centroides al diccinario de centroides de la clase Tracker
+                self.autos_detectados[self.car_id] = (carro)  # se agrega el objeto al diccionario de autos detectados de la calse Tracker
+                self.car_id = self.car_id +1 # Se incrementa el contador de IDs
             
           
-        return self.autos_detectados
+        return self.autos_detectados # Se retorna la lisa de objetos automoviles detectados 
 
